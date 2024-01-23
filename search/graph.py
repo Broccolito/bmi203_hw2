3746c5d4527c23391e7fb7d11194a3a008b912da
Broccolito <wanjungu001@gmail.com>
2024-01-23 00:07:48 -0800

    add bfs code

--- search/graph.py
+++ search/graph.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from collections import deque
 
 class Graph:
     """
@@ -13,6 +14,7 @@ class Graph:
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
 
     def bfs(self, start, end=None):
+        
         """
         TODO: write a method that performs a breadth first traversal and pathfinding on graph G
 
@@ -20,9 +22,52 @@ class Graph:
         * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
         * If there is an end node input and a path does not exist, return None
 
+        In this implementation:
+
+        * The graph is checked for emptiness at the beginning.
+        * The existence of the start and end nodes in the graph is verified.
+        * A breadth-first search is performed using a queue.
+        * If an end node is provided, the algorithm keeps track of paths to each node and returns the path to the end node if found.
+        * If no end node is provided, it returns the list of visited nodes in BFS order.
+        * If a path to the end node does not exist, it returns None.
         """
-        return
 
+        # Check if the graph is empty
+        if not self.graph:
+            return None
 
+        # Check if the start node exists in the graph
+        if start not in self.graph:
+            return None
 
+        # Check if the end node is provided and does not exist in the graph
+        if end is not None and end not in self.graph:
+            return None
+
+        # Initialize variables
+        visited = set()
+        queue = deque([start])
+
+        # Dictionary for storing paths
+        paths = {start: [start]}
+
+        while queue:
+            node = queue.popleft()
+            visited.add(node)
+
+            # If end node is found
+            if node == end:
+                return paths[node]
+
+            for neighbor in self.graph.neighbors(node):
+                if neighbor not in visited and neighbor not in queue:
+                    queue.append(neighbor)
+                    paths[neighbor] = paths[node] + [neighbor]
+
+        # If end is None, return the BFS traversal order
+        if end is None:
+            return list(visited)
+
+        # If no path to end exists
+        return None
 
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -10,17 +10,64 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
+
+    g = Graph('./data/tiny_network.adjlist')
+    result = g.bfs('start_node')  # Replace 'start_node' with an actual start node in 'tiny_network.adjlist'
+    expected_number_of_nodes = 5  # Replace with the expected number of nodes in the BFS traversal
+    assert len(result) == expected_number_of_nodes
+
     pass
 
-def test_bfs():
-    """
-    TODO: Write your unit test for your breadth-first 
-    search here. You should generate an instance of a Graph
-    class using the 'citation_network.adjlist' file 
-    and assert that nodes that are connected return 
-    a (shortest) path between them.
+# def test_bfs():
+#     """
+#     TODO: Write your unit test for your breadth-first 
+#     search here. You should generate an instance of a Graph
+#     class using the 'citation_network.adjlist' file 
+#     and assert that nodes that are connected return 
+#     a (shortest) path between them.
     
-    Include an additional test for nodes that are not connected 
-    which should return None. 
-    """
-    pass
+#     Include an additional test for nodes that are not connected 
+#     which should return None. 
+#     """
+
+#     g = Graph('citation_network.adjlist')
+#     start_node = 'node1'  # Replace with actual start node in 'citation_network.adjlist'
+#     end_node = 'node2'    # Replace with actual end node in 'citation_network.adjlist'
+    
+#     # Test for connected nodes
+#     path = g.bfs(start_node, end_node)
+#     assert path is not None
+#     assert path[0] == start_node
+#     assert path[-1] == end_node
+    
+#     # Test for non-connected nodes
+#     non_connected_node = 'isolated_node'  # Replace with an actual node that is not connected
+#     path = g.bfs(start_node, non_connected_node)
+#     assert path is None
+
+#     pass
+
+# def test_bfs_empty_graph():
+#     g = Graph('empty_network.adjlist')  # Make sure this file represents an empty graph
+#     result = g.bfs('any_node')
+#     assert result is None
+
+# def test_bfs_nonexistent_start_node():
+#     g = Graph('normal_network.adjlist')  # Replace with a file that represents a normal graph
+#     result = g.bfs('nonexistent_node')
+#     assert result is None
+
+# def test_bfs_nonexistent_end_node():
+#     g = Graph('normal_network.adjlist')
+#     result = g.bfs('start_node', 'nonexistent_node')  # Replace 'start_node' with an actual node
+#     assert result is None
+
+# def test_bfs_unconnected_graph():
+#     g = Graph('unconnected_network.adjlist')  # Make sure this file represents an unconnected graph
+#     result = g.bfs('start_node')  # Replace 'start_node' with an actual node in the graph
+#     # Assert based on the expected behavior in an unconnected graph
+
+# # Example of a test case that is expected to fail and raise an exception
+# def test_bfs_invalid_file():
+#     with pytest.raises(Exception):
+#         Graph('invalid_file.adjlist')
